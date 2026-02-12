ebcdb95aa9e89786572a08c5b0ad0788d5c6db62
GitHub Action <action@github.com>
2026-02-12 08:17:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/meteors87/assmartlogcheck/MyBundle.kt
+++ src/main/kotlin/com/github/meteors87/assmartlogcheck/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.meteors87.assmartlogcheck
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/meteors87/assmartlogcheck/services/MyProjectService.kt
+++ src/main/kotlin/com/github/meteors87/assmartlogcheck/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.meteors87.assmartlogcheck.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.meteors87.assmartlogcheck.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
